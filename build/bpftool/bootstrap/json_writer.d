52ae7a91a05e6f52eae02b60dd55962dff4f9c4a
thrishajogi <thrishajogi@gmail.com>
2026-08-03 19:30:06 +0530

    chore: ignore generated build artifacts

--- build/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/thrisha/KernelShield/build/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler.h \
- /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
- /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h
