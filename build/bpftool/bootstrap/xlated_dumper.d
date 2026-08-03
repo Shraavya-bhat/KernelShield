52c44fbd38b22758b1c4b93e0991f111968f2bb3
thrishajogi <thrishajogi@gmail.com>
2026-08-03 19:22:12 +0530

    Phase 1.1: Introduce KernelShield event ABI and metadata

---
+++ build/bpftool/bootstrap/xlated_dumper.d
@@ -0,0 +1,25 @@
+/home/thrisha/KernelShield/build/bpftool/bootstrap/xlated_dumper.o: \
+ xlated_dumper.c \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/uapi/linux/bpf.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/types.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/uapi/linux/bpf_common.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf_common.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf_version.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf_legacy.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf_internal.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/err.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/relo_core.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/btf.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/uapi/linux/btf.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/src/kernel/bpf/disasm.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/kernel.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/build_bug.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h \
+ /home/thrisha/KernelShield/third_party/libbpf-bootstrap/bpftool/include/linux/stringify.h \
+ json_writer.h main.h \
+ /home/thrisha/KernelShield/build/bpftool/bootstrap/libbpf/include/bpf/hashmap.h \
+ xlated_dumper.h
